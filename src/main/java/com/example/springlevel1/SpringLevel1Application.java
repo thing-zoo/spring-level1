1c37428c1a25f671c8589ae91261f3c9a552b0a2
thing-zoo <cathy4025@gmail.com>
2023-06-28 19:16:40 +0900

    Initial commit

---
+++ src/main/java/com/example/springlevel1/SpringLevel1Application.java
@@ -0,0 +1,13 @@
+package com.example.springlevel1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLevel1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLevel1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springlevel1/SpringLevel1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlevel1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLevel1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
